0578584f0f56fe709dc19496dee8d1f8ebba6daf
Samuel Collins <samuel.collins@live.co.uk>
2021-09-27 19:44:19 +0100

    Added basic docstring.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 use proc_macro::TokenStream;
 
+/**
+    Enclosed code is only included in dev builds (not when `--release` is specified)
+*/
 #[proc_macro]
 pub fn if_debug(input: TokenStream) -> TokenStream {
     if cfg!(debug_assertions) {
